1d4116559d8e0a3c6ab3618d9b01b3e4898ad6bf
dhiegohenrique <dhiego_henrique20@yahoo.com.br>
2017-06-01 22:08:08 -0300

    adicionando testes e2e

--- src/test/java/br/com/aurum/astrea/IndexPageTest.java
+++ src/test/java/br/com/aurum/astrea/IndexPageTest.java
@@ -3,12 +3,14 @@ package br.com.aurum.astrea;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -74,9 +76,9 @@ public class IndexPageTest {
 	
 //		
 		System.err.println("entrou no teste1");
-//		this.driver = new PhantomJSDriver();
+		this.driver = new PhantomJSDriver();
 //		this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
-//		this.driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.MINUTES);
+		this.driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.MINUTES);
 		System.err.println("entrou no teste2");
 	}
 	
